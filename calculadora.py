f35653012d7f9e286c18ea42c4e00b2bfd37e7c6
PurpleDavi <davi.bertoldo.almeida@escola.pr.gov.br>
2025-07-04 09:48:45 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+# Calculadora simples Davi n7 1E
+print("Vamos somar dois números!")
+num1 = float(input("digite o primeiro número: "))
+num2 = float(input("digite o segundo número: "))
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}.")
---
+++ calculadroa_idade.py
@@ -0,0 +1,5 @@
+#calculadno a idade de um uma pessoa 
+num1 = float(input("digite o ano em que estamos: "))
+num2 = float(input("digite o ano em que nasceu: "))
+subtrair = num1 - num2
+print(f"A subtração de {num1} e {num2} é {subtrair}.")
